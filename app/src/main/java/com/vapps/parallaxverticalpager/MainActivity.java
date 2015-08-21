e25c795ea7836bb8b6be8a2b7b20dcc7fe9b0d54
vg <vivekgidmare@gmail.com>
2015-08-21 23:41:33 +0530

    Initial commit

---
+++ app/src/main/java/com/vapps/parallaxverticalpager/MainActivity.java
@@ -0,0 +1,127 @@
+package com.vapps.parallaxverticalpager;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VerticalViewPager verticalViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        verticalViewPager = (VerticalViewPager) findViewById(R.id.pager_view);
+        verticalViewPager.setOffscreenPageLimit(3);
+        //For Showing some part of next and Prev page//////
+        verticalViewPager.setClipToPadding(false);
+        verticalViewPager.setPadding(0, 0, 0, 150);
+        verticalViewPager.setPageMargin(convertPixelsToDp(-250, MainActivity.this));
+        setUpViewPager();
+    }
+
+    public static int convertPixelsToDp(int px, Activity activity) {
+        Resources resources = activity.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        return Math.round(px / (metrics.densityDpi / 160f));
+    }
+
+    private void setUpViewPager() {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+
+
+        IntroFirst introFirst = new IntroFirst();
+        Bundle bundle = new Bundle();
+        bundle.putInt("imageId", R.drawable.intro_first_);
+        bundle.putInt("introId", R.string.intro_first);
+        introFirst.setArguments(bundle);
+
+        IntroFirst introSecond = new IntroFirst();
+        Bundle bundle1 = new Bundle();
+        bundle1.putInt("imageId", R.drawable.intro_second_);
+        bundle1.putInt("introId", R.string.intro_second);
+        introSecond.setArguments(bundle1);
+
+        IntroFirst introThird = new IntroFirst();
+        Bundle bundle2 = new Bundle();
+        bundle2.putInt("imageId", R.drawable.intro_third_);
+        bundle2.putInt("introId", R.string.intro_third);
+        introThird.setArguments(bundle2);
+
+        adapter.addFragment(introFirst);
+        adapter.addFragment(introSecond);
+        adapter.addFragment(introThird);
+        verticalViewPager.setAdapter(adapter);
+
+    }
+
+    public static class IntroFirst extends Fragment {
+        View viewRoot;
+        ImageView imageView;
+        TextView textViewDesc;
+        int imageDrawableId;
+        int textStringId;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            viewRoot = inflater.inflate(R.layout.intro_pager_view, container,
+                    false);
+            imageDrawableId = getArguments().getInt("imageId", R.drawable.intro_first_);
+            textStringId = getArguments().getInt("introId");
+            return viewRoot;
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            imageView = (ImageView) viewRoot.findViewById(R.id.image_bg);
+            imageView.setImageDrawable(getResources().getDrawable(imageDrawableId));
+            textViewDesc = (TextView) viewRoot.findViewById(R.id.intro_desc);
+            textViewDesc.setText(getResources().getString(textStringId));
+
+
+        }
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment) {
+            mFragments.add(fragment);
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/vapps/parallaxverticalpager/VerticalViewPager.java
@@ -0,0 +1,96 @@
+package com.vapps.parallaxverticalpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by vivek on 21/08/15.
+ */
+public class VerticalViewPager extends ViewPager {
+    public VerticalViewPager(Context context) {
+        super(context);
+        init();
+    }
+
+    public VerticalViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        // The majority of the magic happens here
+//        setPageTransformer(true, new VerticalPageTransformer(R.id.image_bg));
+        setPageTransformer(true, new VerticalPageTransformer(R.id.intro_desc, R.id.image_bg));
+        // The easiest way to get rid of the overscroll drawing that happens on the left and right
+        setOverScrollMode(OVER_SCROLL_NEVER);
+    }
+
+    private class VerticalPageTransformer implements ViewPager.PageTransformer {
+        private int tvId;
+        private int imgId;
+
+        public VerticalPageTransformer(int tvId, int imgId) {
+            this.tvId = tvId;
+            this.imgId = imgId;
+        }
+
+        @Override
+        public void transformPage(View view, float position) {
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+                view.setAlpha(1);
+
+
+                // Counteract the default slide transition
+                view.setTranslationX(view.getWidth() * -position);
+
+                //set Y position to swipe in from top
+                float yPosition = position * view.getHeight();
+                view.setTranslationY(yPosition);
+
+                View parallaxView = view.findViewById(tvId);
+                parallaxView.setTranslationY((float) (position * (view.getHeight() * 0.5)));
+//                View parallaxViewImage = view.findViewById(imgId);
+//                parallaxViewImage.setTranslationY((float) (position * (view.getHeight() * 0.2)));
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(1);
+            }
+        }
+    }
+
+    /**
+     * Swaps the X and Y coordinates of your touch event.
+     */
+    private MotionEvent swapXY(MotionEvent ev) {
+        float width = getWidth();
+        float height = getHeight();
+
+        float newX = (ev.getY() / height) * width;
+        float newY = (ev.getX() / width) * height;
+
+        ev.setLocation(newX, newY);
+
+        return ev;
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercepted = super.onInterceptTouchEvent(swapXY(ev));
+        swapXY(ev); // return touch coordinates to original reference frame for any child views
+        return intercepted;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return super.onTouchEvent(swapXY(ev));
+    }
+}
